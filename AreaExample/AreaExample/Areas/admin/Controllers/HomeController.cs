8074900169a5a97a03df02c88e82f2f77c18c8e7
mehmetadiguzell <76685004+mehmetadiguzell@users.noreply.github.com>
2022-09-18 20:53:25 +0300

    AreaExample

---
+++ AreaExample/AreaExample/Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaExample.Areas.admin.Controllers
+{
+    [Area("yonetim_paneli")] //program.cs'de kendise ait oluşturulan rota da areaName ile aynı isimde olmalıdır
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = TempData["data"];
+            return View();
+        }
+    }
+}
---
+++ AreaExample/AreaExample/Areas/fatura_paneli/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaExample.Areas.fatura_paneli.Controllers
+{
+    [Area("fatura_paneli")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["data"] = "deneme";
+            return RedirectToAction("Index","Home",new {area= "yonetim_paneli" });//arealar arası veri taşıma
+        }
+    }
+}
